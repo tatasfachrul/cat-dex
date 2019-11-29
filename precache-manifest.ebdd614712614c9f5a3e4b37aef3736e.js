c1538dd15a19588bf857aa575b6d39b3ab23f9ed
Tatas Fachrul <tatasfachrul@gmail.com>
2019-11-29 23:46:25 +0700

    Updates

---
+++ precache-manifest.ebdd614712614c9f5a3e4b37aef3736e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd755fb7bd3f7d27a5a9e6ec90c6af37",
+    "url": "/cat-dex/index.html"
+  },
+  {
+    "revision": "15e87e5a1ba54b212c5b",
+    "url": "/cat-dex/static/css/main.cee97ffe.chunk.css"
+  },
+  {
+    "revision": "02a7ce5283d4d2863219",
+    "url": "/cat-dex/static/js/2.32be7ec0.chunk.js"
+  },
+  {
+    "revision": "15e87e5a1ba54b212c5b",
+    "url": "/cat-dex/static/js/main.4067f64e.chunk.js"
+  },
+  {
+    "revision": "60be232ac8ac3588e7f4",
+    "url": "/cat-dex/static/js/runtime-main.16eacd40.js"
+  }
+]);
